7b1b27436ad75e9f224eff2501d202ca060d3e81
sunilkumarbysani <51699442+sunilkumarbysani@users.noreply.github.com>
2019-07-16 11:13:58 +0530

    Update prime no or not.c

--- prime no or not.c
+++ prime no or not.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 int main()
 {
-    int n, i, flag = 0;
+    int m, i, flag = 0;
 
     scanf("%d", &n);
-    for(i = 2; i <= n/2; ++i)
+    for(i = 2; i <= m/2; ++i)
     {
         // condition for nonprime number
-        if(n%i == 0)
+        if(m%i == 0)
         {
             flag = 1;
             break;
         }
     }
-    if (n == 1) 
+    if (m == 1) 
     {
       printf("no");
     }
